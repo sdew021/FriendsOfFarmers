82c1b87fe1cbc339d83b4e2040b89a8bfe599bac
rahul0324 <rahul.kr0324@gmail.com>
2019-03-31 22:40:05 +0530

    added feedback in navigation bar

--- app/src/main/java/com/example/sdew021/friendsofframers/ConsumerActivity.java
+++ app/src/main/java/com/example/sdew021/friendsofframers/ConsumerActivity.java
@@ -170,8 +170,12 @@ public class ConsumerActivity extends AppCompatActivity implements NavigationVie
         } else if (id == R.id.nav_wallet) {
             startActivity(new Intent(this, consumerWallet.class));
 
-//        } else if (id == R.id.nav_help) {
-//            startActivity(new Intent(this, help.class));
+        } else if (id == R.id.nav_help) {
+            startActivity(new Intent(this, help.class));
+
+        } else if (id == R.id.nav_feedback) {
+            startActivity(new Intent(this, feedback.class));
+
 
         } else if(id == R.id.nav_exit){
             /*Intent intent = new Intent(Intent.ACTION_MAIN);
